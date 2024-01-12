066e53f0b582fd532b738e351cced4179e3e65ca
Mujtahid <mujtahid2018@gmail.com>
2024-01-12 19:21:08 +0600

    Update base config.js
    Update base config.js

--- baseConfig.js
+++ baseConfig.js
@@ -2,7 +2,7 @@ var date = new Date();
 var vccd = date.getFullYear() + ("0" + (date.getMonth() + 1)).slice(-2) + ("0" + date.getDate()).slice(-2) + ("0" + date.getHours()).slice(-2) + ("0" + date.getMinutes()).slice(-2) + ("0" + date.getSeconds()).slice(-2);
 const testConfig =
 {
-      "Project": "FanSee UI Automation",
+      "Project": "Aloompa Mobile app UI Automation",
       "Env":"Main",
       "Language":"",
       "elementFinderURL":"https://colorlib.com/etc/regform/colorlib-regform-36/",
